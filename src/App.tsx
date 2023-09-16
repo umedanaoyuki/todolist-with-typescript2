915a70f57aaac968f949b3e84a80f35d29499cca
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-16 22:16:02 +0900

    雛形の作成＆型のtypeの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css'
+import
+import {useState} from "react";
 
 function App() {
 
+    const [todo, setTodos] = useState<Todo[]>([]);
+
+    type Todo = {
+        inputValue: string;
+        id: number;
+        checked: boolean;
+    };
+
     return (
     <div className="App">
-      testtestkasdkjsnb.
+      <div>
+          <h2>Todoリスト with Typescript</h2>
+          <form onSubmit={() => {}}>
+              <input className="inputText" type="text" onChange={() => {}}></input>
+              <input type="submit" value="作成" className="submitButton"></input>
+          </form>
+
+      </div>
     </div>
   )
 }
